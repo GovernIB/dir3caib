59dd530ef386f523d65dd43b3661f06c1a09501f
mgonzalez-ibit <mgonzalez-ibit@975a77f3-22d7-4d7e-a5ba-5c5de07dca81>
2015-09-23 10:34:04 +0000

    MID: MSG: Regenerate api catalogo
    git-svn-id: https://svn.code.sf.net/p/dir3caib/code/dir3caib-core/branches/dir3caib-1.0@98 975a77f3-22d7-4d7e-a5ba-5c5de07dca81

---
+++ ws/dir3caib-api/src/main/java/es/caib/dir3caib/ws/api/catalogo/ObtenerCatServicio.java
@@ -0,0 +1,32 @@
+
+package es.caib.dir3caib.ws.api.catalogo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for obtenerCatServicio complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="obtenerCatServicio">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "obtenerCatServicio")
+public class ObtenerCatServicio {
+
+
+}
