ee4a490b1c8f325fc57c75491e579a3c5a5850b3
earrivi-ibit <earrivi-ibit@975a77f3-22d7-4d7e-a5ba-5c5de07dca81>
2015-10-14 11:15:49 +0000

    -Mejoras en el Test para probar los métodos Ws del API
    git-svn-id: https://svn.code.sf.net/p/dir3caib/code/dir3caib-core/branches/dir3caib-1.0@117 975a77f3-22d7-4d7e-a5ba-5c5de07dca81

---
+++ ws/dir3caib-api/src/test/java/es/caib/dir3caib/ws/api/test/Dir3CaibTestUtils.java
@@ -0,0 +1,76 @@
+package es.caib.dir3caib.ws.api.test;
+
+import es.caib.dir3caib.ws.api.unidad.Dir3CaibObtenerUnidadesWs;
+import es.caib.dir3caib.ws.api.unidad.Dir3CaibObtenerUnidadesWsService;
+
+import javax.xml.ws.BindingProvider;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by earrivi on 14/10/2015.
+ */
+public class Dir3CaibTestUtils{
+
+    public static final String OBTENERUNIDADES = "Dir3CaibObtenerUnidades";
+
+    private static Properties testProperties = new Properties();
+
+    static {
+        // Propietats del Servidor
+        try {
+            System.out.println(new File(".").getAbsolutePath());
+            testProperties.load(new FileInputStream("test.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getEndPoint(String api) {
+        return testProperties.getProperty("test_host") + api;
+    }
+
+    public static String getTestAppUserName() {
+        return testProperties.getProperty("test_usr");
+    }
+
+    public static String getTestAppPassword() {
+
+        return testProperties.getProperty("test_pwd");
+    }
+
+    public static void configAddressUserPassword(String usr, String pwd, String endpoint,
+                                                 Object api) {
+
+        Map<String, Object> reqContext = ((BindingProvider) api).getRequestContext();
+        reqContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpoint);
+        reqContext.put(BindingProvider.USERNAME_PROPERTY, usr);
+        reqContext.put(BindingProvider.PASSWORD_PROPERTY, pwd);
+    }
+
+
+    public static Dir3CaibObtenerUnidadesWs getObtenerUnidadesApi(boolean setUsrPwd)
+            throws Exception {
+        final String endpoint = getEndPoint(OBTENERUNIDADES);
+
+        System.out.println("endPoint: " + endpoint);
+
+
+        URL wsdlLocation = new URL(endpoint + "?wsdl");
+        System.out.println("WSDL: " + wsdlLocation);
+
+        Dir3CaibObtenerUnidadesWsService service = new Dir3CaibObtenerUnidadesWsService(
+                wsdlLocation);
+
+        Dir3CaibObtenerUnidadesWs api = service.getDir3CaibObtenerUnidadesWs();
+
+        if (setUsrPwd) {
+            configAddressUserPassword(getTestAppUserName(), getTestAppPassword(), endpoint, api);
+        }
+
+        return api;
+    }
+}
--- ws/dir3caib-api/src/test/java/es/caib/dir3caib/ws/api/test/TestUnidadesWs.java
+++ ws/dir3caib-api/src/test/java/es/caib/dir3caib/ws/api/test/TestUnidadesWs.java
@@ -1,93 +1,22 @@
 package es.caib.dir3caib.ws.api.test;
 
 import es.caib.dir3caib.ws.api.unidad.Dir3CaibObtenerUnidadesWs;
-import es.caib.dir3caib.ws.api.unidad.Dir3CaibObtenerUnidadesWsService;
 import es.caib.dir3caib.ws.api.unidad.UnidadTF;
 import org.junit.Test;
 
-import javax.xml.ws.BindingProvider;
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.URL;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 /**
  * Created by mgonzalez on 03/09/2015.
  */
-public class TestUnidadesWs {
-
-    public static final String OBTENERUNIDADES = "Dir3CaibObtenerUnidades";
-
-    private static Properties testProperties = new Properties();
-
-    static {
-        // Propietats del Servidor
-        try {
-            System.out.println(new File(".").getAbsolutePath());
-            testProperties.load(new FileInputStream("test.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String getEndPoint(String api) {
-        return testProperties.getProperty("test_host") + api;
-    }
-
-    public static String getTestAppUserName() {
-        return testProperties.getProperty("test_usr");
-    }
-
-    public static String getTestAppPassword() {
-
-        return testProperties.getProperty("test_pwd");
-    }
-
-    @Test
-    public void test() {
-        main(new String[] {});
-    }
-
+public class TestUnidadesWs extends Dir3CaibTestUtils{
 
 
     @Test
-    public static Dir3CaibObtenerUnidadesWs getObtenerUnidadesApi(boolean setUsrPwd)
-            throws Exception {
-        final String endpoint = getEndPoint(OBTENERUNIDADES);
-
-        System.out.println("endPoint: " + endpoint);
-
-
-        URL wsdlLocation = new URL(endpoint + "?wsdl");
-        System.out.println("WSDL: " + wsdlLocation);
-
-        Dir3CaibObtenerUnidadesWsService service = new Dir3CaibObtenerUnidadesWsService(
-                wsdlLocation);
-
-        Dir3CaibObtenerUnidadesWs api = service.getDir3CaibObtenerUnidadesWs();
-
-        if (setUsrPwd) {
-            configAddressUserPassword(getTestAppUserName(), getTestAppPassword(), endpoint, api);
-        }
-
-        return api;
-    }
-
-    public static void configAddressUserPassword(String usr, String pwd, String endpoint,
-                                                 Object api) {
-
-        Map<String, Object> reqContext = ((BindingProvider) api).getRequestContext();
-        reqContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpoint);
-        reqContext.put(BindingProvider.USERNAME_PROPERTY, usr);
-        reqContext.put(BindingProvider.PASSWORD_PROPERTY, pwd);
-    }
-
-    public static void main(String[] args) {
+    public void obtenerArbolUnidadesDestinatarias() {
       try{
           Dir3CaibObtenerUnidadesWs apiUnidades = getObtenerUnidadesApi(true);
-          List<UnidadTF> destinatarias = apiUnidades.obtenerArbolUnidadesDestinatarias("L01070407");
+          List<UnidadTF> destinatarias = apiUnidades.obtenerArbolUnidadesDestinatarias("U03600001");
             System.out.println("DESTINATRIAS " + destinatarias.size());
           for (UnidadTF unidadTF : destinatarias) {
             System.out.println(unidadTF.getCodigo() + "\t\t"
