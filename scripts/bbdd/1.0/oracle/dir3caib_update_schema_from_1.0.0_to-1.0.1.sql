b1b5c7b2b2568fe11e16958fe691b4a4bb42c2e9
mgonzalez-ibit <mgonzalez-ibit@975a77f3-22d7-4d7e-a5ba-5c5de07dca81>
2015-11-03 12:59:37 +0000

    MID: 0000264 MSG: Sincronitzacio REGWEB amb DIR3CAIB controlar llibres que no quedin a organigrama sense oficines
    git-svn-id: https://svn.code.sf.net/p/dir3caib/code/dir3caib-core/branches/dir3caib-1.0@123 975a77f3-22d7-4d7e-a5ba-5c5de07dca81

--- scripts/bbdd/1.0/oracle/dir3caib_update_schema_from_1.0.0_to-1.0.1.sql
+++ scripts/bbdd/1.0/oracle/dir3caib_update_schema_from_1.0.0_to-1.0.1.sql
@@ -1,4 +1,3 @@
---20/10/2015 cambiar fechaimportacion de date a timestamp. (ORACLE)
---Pendiente de aplicar en la APB Y CAIB
+--20/10/2015 cambiar fechaimportacion de date a timestamp.
 ALTER TABLE dir_unidad MODIFY fechaimportacion TIMESTAMP;
 ALTER TABLE dir_oficina MODIFY fechaimportacion TIMESTAMP;
--- scripts/bbdd/1.0/postgresql/dir3caib_update_schema_from_1.0.0_to-1.0.1.sql
+++ scripts/bbdd/1.0/postgresql/dir3caib_update_schema_from_1.0.0_to-1.0.1.sql
@@ -1,4 +1,3 @@
---20/10/2015 cambiar fechaimportacion de date a timestamp. Debemos versionar a la 1.0.1
---(PENDIENTE DE APLICAR EN PORTS Y CAIB)
+--20/10/2015 cambiar fechaimportacion de date a timestamp.
 ALTER TABLE dir_unidad ALTER COLUMN fechaimportacion TYPE timestamp;
 ALTER TABLE dir_oficina ALTER COLUMN fechaimportacion TYPE timestamp;
