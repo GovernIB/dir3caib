238fcd70a51762543980724ea4175fa4b8c86b69
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2016-07-29 08:39:30 +0200

    Arbol dir3 UIB

--- persistence/src/main/java/es/caib/dir3caib/persistence/ejb/ArbolLocal.java
+++ persistence/src/main/java/es/caib/dir3caib/persistence/ejb/ArbolLocal.java
@@ -11,7 +11,8 @@ import javax.ejb.Local;
 public interface ArbolLocal {
 
     /**
-     * Metodo que devuelve una estructura de nodos que representan un árbol de unidades
+     * Método que monta el arbol de nodos de la unidad indicada.
+     * Se emplea en dir3caib para mostrar el arbol de una unidad
      *
      * @param idUnidad    unidad raiz de la que partimos.
      * @param nodo        nodo en el que se montará todo el árbol.
@@ -21,5 +22,25 @@ public interface ArbolLocal {
      */
     public void arbolUnidades(String idUnidad, Nodo nodo, String estado, boolean conOficinas) throws Exception;
 
+    /**
+     * Metodo que devuelve una estructura de nodos que representan un árbol de oficinas
+     *
+     * @param idOficina oficina raiz de la que partimos.
+     * @return Nodo (árbol)
+     */
     public void arbolOficinas(String idOficina, Nodo nodo, String estado) throws Exception;
+
+    /**
+     * Método que devuelve el árbol completo(ascendentes y descendentes) de la unidad indicada.
+     * Primero obtiene los descendientes llamando a arbolUnidades y después obtiene los ascendentes.
+     * Es el que se enmplea para mostrar el arbol en la búsqueda de organismos destinatarios de regweb3
+     *
+     * @param idUnidad    unidad  de la que partimos.
+     * @param nodo        nodo en el que se montará todo el árbol.
+     * @param estado      estado de las unidades que queremos mostrar en el arbol.
+     * @param conOficinas indica si se quieren incluir las oficinas en el organigrama (es para mostrar el arbol en las
+     *                    búsquedas de organismo destinatario de regweb3)
+     * @return Nodo (arbol)
+     */
+    public void arbolUnidadesAscendentes(String idUnidad, Nodo nodo, String estado, boolean conOficinas) throws Exception;
 }
