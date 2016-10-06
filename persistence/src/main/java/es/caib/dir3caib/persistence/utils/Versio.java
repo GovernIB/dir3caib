8c5a5bf33844724002a45adf78955b1ee8fb56ec
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2016-10-06 08:50:38 +0200

    cambio a versión 1.0.6

--- persistence/src/main/java/es/caib/dir3caib/persistence/utils/Versio.java
+++ persistence/src/main/java/es/caib/dir3caib/persistence/utils/Versio.java
@@ -3,5 +3,5 @@ package es.caib.dir3caib.persistence.utils;
 * Codi autogenerat a partir del fitxer Versio.java.template.
 */
 public final class Versio {
-  public static final String VERSIO="1.0.5";
+  public static final String VERSIO = "1.0.6";
 }
