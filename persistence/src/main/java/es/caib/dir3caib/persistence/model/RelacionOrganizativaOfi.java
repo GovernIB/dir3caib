3111a133ca59521d80589b89a550f7325b5e15fc
earrivi <earrivi@fundaciobit.org>
2017-01-31 10:05:59 +0100

    - Campos a NOT NULL de tablas DIR_RELACIONORGANIZATIVAOFI y DIR_RELACIONSIROFI

--- persistence/src/main/java/es/caib/dir3caib/persistence/model/RelacionOrganizativaOfi.java
+++ persistence/src/main/java/es/caib/dir3caib/persistence/model/RelacionOrganizativaOfi.java
@@ -12,115 +12,113 @@ import java.io.Serializable;
  * Esta relación representa la dependencia funcional entre unidades y oficinas
  */
 @Table(name = "DIR_RELACIONORGANIZATIVAOFI",
-    uniqueConstraints= @UniqueConstraint(columnNames={"CODOFICINA", "CODUNIDAD"}))
+        uniqueConstraints = @UniqueConstraint(columnNames = {"CODOFICINA", "CODUNIDAD"}))
 @org.hibernate.annotations.Table(appliesTo = "DIR_RELACIONORGANIZATIVAOFI", indexes = {
-    @Index(name="DIR_RELORGANOFI_CATESTENT_FK_I", columnNames = {"ESTADO"}),
-    @Index(name="DIR_UNIDAD_RELORGOFI_FK_I", columnNames = {"CODUNIDAD"}),
-    @Index(name="DIR_OFICINA_RELORGOFI_FK_I", columnNames = {"CODOFICINA"})
+        @Index(name = "DIR_RELORGANOFI_CATESTENT_FK_I", columnNames = {"ESTADO"}),
+        @Index(name = "DIR_UNIDAD_RELORGOFI_FK_I", columnNames = {"CODUNIDAD"}),
+        @Index(name = "DIR_OFICINA_RELORGOFI_FK_I", columnNames = {"CODOFICINA"})
 })
 @Entity
-@SequenceGenerator(name="generator",sequenceName = "DIR_SEQ_ALL", allocationSize=1)
+@SequenceGenerator(name = "generator", sequenceName = "DIR_SEQ_ALL", allocationSize = 1)
 public class RelacionOrganizativaOfi implements Serializable {
 
-  private Long id;
-	private Oficina oficina;
-	private Unidad unidad;
-	private CatEstadoEntidad estado;
+    private Long id;
+    private Oficina oficina;
+    private Unidad unidad;
+    private CatEstadoEntidad estado;
 
-	public RelacionOrganizativaOfi(){
+    public RelacionOrganizativaOfi() {
 
-	}
+    }
 
-	public void finalize() throws Throwable {
+    public void finalize() throws Throwable {
 
-	}
+    }
 
-  /**
-   * @return the oficina
-   */
+    /**
+     * @return the oficina
+     */
 
-  @ManyToOne(cascade=CascadeType.PERSIST)
-  @JoinColumn (name="CODOFICINA")
-  @ForeignKey(name="DIR_RELORGOFI_CATOFI_FK")
-  public Oficina getOficina() {
-    return oficina;
-  }
+    @ManyToOne(cascade = CascadeType.PERSIST,optional = false)
+    @JoinColumn(name = "CODOFICINA")
+    @ForeignKey(name = "DIR_RELORGOFI_CATOFI_FK")
+    public Oficina getOficina() {
+        return oficina;
+    }
 
-  /**
-   * @param oficina the codOficina to set
-   */
-  public void setOficina(Oficina oficina) {
-    this.oficina = oficina;
-  }
+    /**
+     * @param oficina the codOficina to set
+     */
+    public void setOficina(Oficina oficina) {
+        this.oficina = oficina;
+    }
 
-  /**
-   * @return the unidad
-   */
+    /**
+     * @return the unidad
+     */
 
-  @ManyToOne()
-  @JoinColumn (name="CODUNIDAD")
-  @ForeignKey(name="DIR_RELORGOFI_CATUNIDAD_FK")
-  public Unidad getUnidad() {
-    return unidad;
-  }
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "CODUNIDAD")
+    @ForeignKey(name = "DIR_RELORGOFI_CATUNIDAD_FK")
+    public Unidad getUnidad() {
+        return unidad;
+    }
 
-  /**
-   * @param unidad the unidad to set
-   */
-  public void setUnidad(Unidad unidad) {
-    this.unidad = unidad;
-  }
+    /**
+     * @param unidad the unidad to set
+     */
+    public void setUnidad(Unidad unidad) {
+        this.unidad = unidad;
+    }
 
-  /**
-   * @return the estado
-   */
-  @ManyToOne(cascade=CascadeType.PERSIST)
-  @JoinColumn(name="ESTADO")
-  @ForeignKey(name="DIR_RELORGANOFI_CATESTENT_FK")
-  public CatEstadoEntidad getEstado() {
-    return estado;
-  }
+    /**
+     * @return the estado
+     */
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "ESTADO")
+    @ForeignKey(name = "DIR_RELORGANOFI_CATESTENT_FK")
+    public CatEstadoEntidad getEstado() {
+        return estado;
+    }
 
-  /**
-   * @param estado the estado to set
-   */
-  public void setEstado(CatEstadoEntidad estado) {
-    this.estado = estado;
-  }
-  
-  
-  
-  
-  @Id
-  @JoinColumn(name="ID")
-  @GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "generator")
-  public Long getId() {
-    return id;
-  }
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(CatEstadoEntidad estado) {
+        this.estado = estado;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Id
+    @JoinColumn(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
+    public Long getId() {
+        return id;
+    }
 
-    RelacionOrganizativaOfi that = (RelacionOrganizativaOfi) o;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    if (!estado.equals(that.estado)) return false;
-    if (!oficina.equals(that.oficina)) return false;
-    if (!unidad.equals(that.unidad)) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    return true;
-  }
+        RelacionOrganizativaOfi that = (RelacionOrganizativaOfi) o;
 
-  @Override
-  public int hashCode() {
-    int result = oficina.hashCode();
-    result = 31 * result + unidad.hashCode();
-    result = 31 * result + estado.hashCode();
-    return result;
-  }
+        if (!estado.equals(that.estado)) return false;
+        if (!oficina.equals(that.oficina)) return false;
+        if (!unidad.equals(that.unidad)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = oficina.hashCode();
+        result = 31 * result + unidad.hashCode();
+        result = 31 * result + estado.hashCode();
+        return result;
+    }
 }
--- persistence/src/main/java/es/caib/dir3caib/persistence/model/RelacionSirOfi.java
+++ persistence/src/main/java/es/caib/dir3caib/persistence/model/RelacionSirOfi.java
@@ -12,114 +12,114 @@ import java.io.Serializable;
  * Esta relación es la referente al intercambio de Registros (SIR). Determina que oficina da servicio a una unidad.
  */
 @Table(name = "DIR_RELACIONSIROFI",
-  uniqueConstraints= @UniqueConstraint(columnNames={"CODOFICINA", "CODUNIDAD"}))
+        uniqueConstraints = @UniqueConstraint(columnNames = {"CODOFICINA", "CODUNIDAD"}))
 @org.hibernate.annotations.Table(appliesTo = "DIR_RELACIONSIROFI", indexes = {
-    @Index(name="DIR_RELSIROFI_CATESTENTI_FK_I", columnNames = {"ESTADO"}),
-    @Index(name="DIR_UNIDAD_RELSIROFI_FK_I", columnNames = {"CODUNIDAD"}),
-    @Index(name="DIR_OFICINA_RELSIROFI_FK_I", columnNames = {"CODOFICINA"})
+        @Index(name = "DIR_RELSIROFI_CATESTENTI_FK_I", columnNames = {"ESTADO"}),
+        @Index(name = "DIR_UNIDAD_RELSIROFI_FK_I", columnNames = {"CODUNIDAD"}),
+        @Index(name = "DIR_OFICINA_RELSIROFI_FK_I", columnNames = {"CODOFICINA"})
 })
 @Entity
-@SequenceGenerator(name="generator",sequenceName = "DIR_SEQ_ALL", allocationSize=1)
+@SequenceGenerator(name = "generator", sequenceName = "DIR_SEQ_ALL", allocationSize = 1)
 public class RelacionSirOfi implements Serializable {
 
-  private Long id;
-	private Oficina oficina;
-	private Unidad unidad;
-	private CatEstadoEntidad estado;
+    private Long id;
+    private Oficina oficina;
+    private Unidad unidad;
+    private CatEstadoEntidad estado;
 
-	public RelacionSirOfi(){
+    public RelacionSirOfi() {
 
-	}
+    }
 
-	public void finalize() throws Throwable {
+    public void finalize() throws Throwable {
 
-	}
-	
-	
-	@Id
-  @Column(name = "ID", nullable = false)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "generator")
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  /**
-   * @return the oficina
-   */
-
-  @ManyToOne(cascade=CascadeType.PERSIST)
-  @JoinColumn (name="CODOFICINA")
-  @ForeignKey(name="DIR_RELSIROFI_CATOFI_FK")
-  public Oficina getOficina() {
-    return oficina;
-  }
-
-  /**
-   * @param oficina the oficina to set
-   */
-  public void setOficina(Oficina oficina) {
-    this.oficina = oficina;
-  }
-
-  /**
-   * @return the unidad
-   */
-
-  @ManyToOne()
-  @JoinColumn (name="CODUNIDAD")
-  @ForeignKey(name="DIR_RELSIROFI_CATUNIDAD_FK")
-  public Unidad getUnidad() {
-    return unidad;
-  }
-
-  /**
-   * @param unidad the unidad to set
-   */
-  public void setUnidad(Unidad unidad) {
-    this.unidad = unidad;
-  }
-
-  /**
-   * @return the estado
-   */
-  @ManyToOne(cascade=CascadeType.PERSIST)
-  @JoinColumn(name="ESTADO")
-  @ForeignKey(name="DIR_RELSIROFI_CATESTENTI_FK")
-  public CatEstadoEntidad getEstado() {
-    return estado;
-  }
-
-  /**
-   * @param estado the estado to set
-   */
-  public void setEstado(CatEstadoEntidad estado) {
-    this.estado = estado;
-  }
+    }
 
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Id
+    @Column(name = "ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
+    public Long getId() {
+        return id;
+    }
 
-    RelacionSirOfi that = (RelacionSirOfi) o;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    if (!estado.equals(that.estado)) return false;
-    if (!oficina.equals(that.oficina)) return false;
-    if (!unidad.equals(that.unidad)) return false;
+    /**
+     * @return the oficina
+     */
 
-    return true;
-  }
+    @ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+    @JoinColumn(name = "CODOFICINA")
+    @ForeignKey(name = "DIR_RELSIROFI_CATOFI_FK")
+    public Oficina getOficina() {
+        return oficina;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = oficina.hashCode();
-    result = 31 * result + unidad.hashCode();
-    result = 31 * result + estado.hashCode();
-    return result;
-  }
+    /**
+     * @param oficina the oficina to set
+     */
+    public void setOficina(Oficina oficina) {
+        this.oficina = oficina;
+    }
+
+    /**
+     * @return the unidad
+     */
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "CODUNIDAD", nullable = false)
+    @ForeignKey(name = "DIR_RELSIROFI_CATUNIDAD_FK")
+    public Unidad getUnidad() {
+        return unidad;
+    }
+
+    /**
+     * @param unidad the unidad to set
+     */
+    public void setUnidad(Unidad unidad) {
+        this.unidad = unidad;
+    }
+
+    /**
+     * @return the estado
+     */
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "ESTADO")
+    @ForeignKey(name = "DIR_RELSIROFI_CATESTENTI_FK")
+    public CatEstadoEntidad getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(CatEstadoEntidad estado) {
+        this.estado = estado;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RelacionSirOfi that = (RelacionSirOfi) o;
+
+        if (!estado.equals(that.estado)) return false;
+        if (!oficina.equals(that.oficina)) return false;
+        if (!unidad.equals(that.unidad)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = oficina.hashCode();
+        result = 31 * result + unidad.hashCode();
+        result = 31 * result + estado.hashCode();
+        return result;
+    }
 }
