b4c3f05c4fa90794c0fe546d2724a3783e277278
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2017-10-04 13:42:22 +0200

    Actualitzada documentació

--- persistence/src/main/java/es/caib/dir3caib/persistence/utils/CodigoValor.java
+++ persistence/src/main/java/es/caib/dir3caib/persistence/utils/CodigoValor.java
@@ -4,6 +4,9 @@ package es.caib.dir3caib.persistence.utils;
  * Created 30/07/14 14:33
  *
  * @author mgonzalez
+ *
+ * Clase que representa la tupla(id-Descripción) de un objeto del catálogo de dir3caib(Ambitoterritorial, Pais, etc)
+ * Se emplea en los métodos rest para devolver un conjunto de datos reducidos.
  */
 public class CodigoValor {
 
