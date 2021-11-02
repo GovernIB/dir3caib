6050ec77787c41e1e2627dc16c272f1f3aee9996
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2021-11-02 15:40:40 +0100

    Script per esborrar el cataleg de dades.

---
+++ scripts/development/borrar_datos_catalogo_postgres.sql
@@ -0,0 +1,19 @@
+
+TRUNCATE TABLE dir_catmotivoextincion  CASCADE;
+TRUNCATE TABLE dir_cattipocontacto  CASCADE;
+TRUNCATE TABLE dir_catjerarquiaoficina  CASCADE;
+TRUNCATE TABLE dir_catprovincia  CASCADE;
+TRUNCATE TABLE dir_catservicio  CASCADE;
+TRUNCATE TABLE dir_catcomunidadautonoma  CASCADE;
+TRUNCATE TABLE dir_catambitoterritorial  CASCADE;
+TRUNCATE TABLE dir_cattipoentidadpublica  CASCADE;
+TRUNCATE TABLE dir_catpais  CASCADE;
+TRUNCATE TABLE dir_cattipovia  CASCADE;
+TRUNCATE TABLE dir_catentidadgeografica  CASCADE;
+TRUNCATE TABLE dir_catniveladministracion  CASCADE;
+TRUNCATE TABLE dir_catisla  CASCADE;
+TRUNCATE TABLE dir_catlocalidad  CASCADE;
+TRUNCATE TABLE dir_cattipounidadorganica  CASCADE;
+TRUNCATE TABLE dir_catestadoentidad  CASCADE;
+TRUNCATE TABLE dir_sincronizacion  CASCADE;
+ALTER SEQUENCE DIR_SEQ_ALL RESTART WITH 1;
