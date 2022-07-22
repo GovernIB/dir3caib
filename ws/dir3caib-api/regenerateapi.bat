535e24e20990169eeff6ed333e381802449a5968
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2022-07-22 12:42:28 +0200

    Arreglado regenerateapi

--- ws/dir3caib-api/regenerateapi.bat
+++ ws/dir3caib-api/regenerateapi.bat
@@ -1,5 +1,10 @@
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
+rem set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m -Dhttps.protocols=TLSv1.2
+rem mvn -Dhttps.protocols=TLSv1.2 clean install -DskipTests -Dregenerateapi -e
 
-set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m
 
-mvn -Dhttps.protocols=TLSv1.2 clean install -DskipTests -Dregenerateapi -e
+rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_80
+rem set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m -Dhttps.protocols=TLSv1.2
+
+mvn clean install -DskipTests -Dregenerateapi
 
